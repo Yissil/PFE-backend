2d70253c3ddb1a0315d6ad4f7729991b08097636
Youssef Issil <67612945+Yissil@users.noreply.github.com>
2024-08-09 18:33:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,11 +11,27 @@ app = Flask(__name__)
 @app.route('/data', methods=['POST'])
 def post_data():
     data = request.json
+
     cursor = conn.cursor()
+    
+    # Prepare the SQL query and values based on the payload
     cursor.execute(
-        "INSERT INTO data (id, sensor, value_) VALUES (?, ?, ?)",
-        (data['id'], data['sensor'], data['value_'])
+        """
+        INSERT INTO data (HUMIDITY, TEMPERATURE1, TEMPERATURE2, TEMPERATURE3, PRESSURE, HEARTRATE, SPO2, IPV)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """,
+        (
+            data.get('humidity'),
+            data.get('temperature1'),
+            data.get('temperature2'),
+            data.get('temperature3'),
+            data.get('pressure'),
+            data.get('heartrate'),
+            data.get('spo2'),
+            data.get('ipv')
+        )
     )
+    
     conn.commit()
 
     # Log the POST request data
